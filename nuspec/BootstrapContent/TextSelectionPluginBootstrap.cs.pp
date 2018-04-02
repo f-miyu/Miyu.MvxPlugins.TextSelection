dee018690e0829511c3d9fc5680fa109fea67fcf
[mediba] Fumiyuki Matsushima <f-matsushima@mediba.jp>
2018-04-02 19:43:13 +0900

    nuspec作成

---
+++ nuspec/BootstrapContent/TextSelectionPluginBootstrap.cs.pp
@@ -0,0 +1,9 @@
+using MvvmCross.Platform.Plugins;
+
+namespace $rootnamespace$.Bootstrap
+{
+    public class TextSelectionPluginBootstrap
+        : MvxPluginBootstrapAction<MvvmCross.Plugins.TextSelection.PluginLoader>
+    {
+    }
+}
---
+++ nuspec/iOSBootstrapContent/TextSelectionPluginBootstrap.cs.pp
@@ -0,0 +1,9 @@
+using MvvmCross.Platform.Plugins;
+
+namespace $rootnamespace$.Bootstrap
+{
+    public class TextSelectionPluginBootstrap
+        : MvxLoaderPluginBootstrapAction<MvvmCross.Plugins.TextSelection.PluginLoader, MvvmCross.Plugins.TextSelection.iOS.Plugin>
+    {
+    }
+}
